2fa96d5c35266f7bada4fd77912a6f6a4ae77766
Simon Conseil <simon.conseil@univ-lyon1.fr>
2026-06-09 22:48:18 +0200

    add config files

---
+++ galleria.conf.py
@@ -0,0 +1,11 @@
+source = 'src'
+destination = 'site/galleria'
+
+theme = 'galleria'
+img_size = (800, 600)
+thumb_size = (250, 200)
+use_orig = True
+show_map = True
+
+links = [('Github', 'https://github.com/saimn/sigal'),
+         ('Docs', 'http://sigal.saimon.org/en/latest/')]
---
+++ photoswipe.conf.py
@@ -0,0 +1,10 @@
+source = 'src'
+destination = 'site/photoswipe'
+
+theme = 'photoswipe'
+img_size = (800, 600)
+thumb_size = (200, 150)
+use_orig = True
+
+links = [('Github', 'https://github.com/saimn/sigal'),
+         ('Docs', 'http://sigal.saimon.org/en/latest/')]
